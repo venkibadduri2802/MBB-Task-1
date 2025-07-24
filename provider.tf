bc6916b5abf8a5d77bac3d71cc136772597c120c
venkibadduri2802 <130340932+venkibadduri2802@users.noreply.github.com>
2025-07-24 16:01:30 +0800

    Add files via upload

---
+++ provider.tf
@@ -0,0 +1,13 @@
+<<<<<<< HEAD
+provider "aws" {
+  region     = "ap-south-1" # Mumbai region
+  access_key = "XXXXXXXXXXXXXXX"
+  secret_key = "YYYYYYYYYYYYYYY"
+}
+=======
+provider "aws" {
+  region     = "ap-south-1" # Mumbai region
+  access_key = "AKIAYAKEDGFDBULYZL7Q"
+  secret_key = "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"
+}
+>>>>>>> 75068c1 (Initial commit: Terraform AWS infra with VPC, EC2, and S3)
